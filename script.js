0b7d48706ff1816110cb90cee770f6a871f63cfb
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2023-02-13 12:26:38 +0100

    feedback Denis

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ let notificationCounter = notifications.length;
 
 //sets all notifications to read
 document.querySelector('header p').addEventListener('click', function () {
+  //sets the notification number
   notificationCounter = 0;
   document.querySelector('.notification-num').innerHTML = notificationCounter;
+  //removes background color from each notification
   notifications.forEach((article) => {
     article.classList.remove('active');
+  });
+  //removes dots from each notification
+  let dots = document.querySelectorAll('p .dot');
+  dots.forEach((dot) => {
+    dot.style.display = 'none';
   });
 });
 
@@ -16,10 +23,13 @@ notifications.forEach((article) => {
   article.addEventListener('click', function () {
     //checks if notification is read
     if (article.className === 'active') {
+      //increase the notification counter
       notificationCounter--;
-      console.log(notificationCounter);
       document.querySelector('.notification-num').innerHTML = notificationCounter;
+      //removes background color of notification
       article.classList.remove('active');
+      //removes dot of notification
+      document.querySelector(`#${article.id} .dot`).style.display = 'none';
     }
   });
 });
